e40cf7e58ecba16e3412293bdf8b088f374f50d4
uniend <dudnsgldns@naver.com>
2023-07-04 01:31:58 +0900

    전역상태 1차 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import router from './router/routing';
+import {Provider} from 'react-redux'
+import store from './store/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}> 
       <RouterProvider router={router}/>
-    </>
+    </Provider>
   );
 }
 
